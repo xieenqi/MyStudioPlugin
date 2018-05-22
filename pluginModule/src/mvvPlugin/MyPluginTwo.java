6afa914fc6ffe9da33dc82e50238b575b4c4277f
juno <329191055@qq.com>
2018-05-22 13:52:26 +0800

    插件 menu位置确定

---
+++ pluginModule/src/mvvPlugin/MyPluginTwo.java
@@ -0,0 +1,12 @@
+package mvvPlugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class MyPluginTwo extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
--- src/TestAction.java
+++ src/TestAction.java
@@ -5,13 +5,13 @@ public class TestAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        // TODO: insert action logic here Edit Configurations
     }
 
-    @Override
-    protected void buildDialog(Project project, PsiDirectory psiDirectory, CreateFileFromTemplateDialog.Builder builder) {
-        builder.setTitle("Auto Generate")
-                .addKind("Activity", PlatformIcons.CLASS_ICON, MODE_SINGLE_ACTIVITY)
-                .addKind("Fragment", PlatformIcons.CLASS_ICON, MODE_FRAGMENT);
-    }
+//    @Override
+//    protected void buildDialog(Project project, PsiDirectory psiDirectory, CreateFileFromTemplateDialog.Builder builder) {
+//        builder.setTitle("Auto Generate")
+//                .addKind("Activity", PlatformIcons.CLASS_ICON, "MODE_SINGLE_ACTIVITY")
+//                .addKind("Fragment", PlatformIcons.CLASS_ICON, "MODE_FRAGMENT");
+//    }
 }
